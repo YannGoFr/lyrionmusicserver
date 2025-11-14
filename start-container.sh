05d0a879fc5da2f7d0984aaf9d4d678c0293da62
YannGoFr <yann.godest@gmail.com>
2025-11-14 06:41:18 +0100

    Update start-container.sh
    simplification du script de démarrage

--- start-container.sh
+++ start-container.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 #Set user and group
-umask 000
-#PUID=${PUID:-`id -u squeezeboxserver`}
-#PGID=${PGID:-`id -g squeezeboxserver`}
+umask 0002
+PUID=${PUID:-`id -u squeezeboxserver`}
+PGID=${PGID:-`id -g squeezeboxserver`}
 
 # Set uid of user squeezeboxserver to $PUID
 echo Set uid of user squeezeboxserver to $PUID
-usermod -o -u $PUID squeezeboxserver
+usermod -o -u "$PUID" squeezeboxserver
 
 # Set id of group squeezeboxserver to $PGID and set gid of user squeezeboxserver to $PGID
 echo Set id of group squeezeboxserver to $PGID
-groupmod -o -g $PGID squeezeboxserver
+groupmod -o -g "$PGID" squeezeboxserver
 echo Set gid of user squeezeboxserver to $PGID
 usermod -g $PGID squeezeboxserver
 
 #Add permissions
-# chown -R squeezeboxserver:squeezeboxserver /config /playlist
-chown -R squeezeboxserver:squeezeboxserver /config
+chown -R squeezeboxserver:squeezeboxserver /config /playlist
 
 if [[ -f /config/custom-init.sh ]]; then
 	echo "Running custom initialization script..."
@@ -28,14 +27,4 @@ echo Starting Lyrion Music Server on port $HTTP_PORT...
 if [[ -n "$EXTRA_ARGS" ]]; then
 	echo "Using additional arguments: $EXTRA_ARGS"
 fi
-
-# Preparing /config folder
-#mkdir -pv /config/prefs /config/logs /config/cache
-mkdir -pv /config/prefs || echo "Erreur lors de la création du répertoire, continuation du script..."
-mkdir -pv /config/logs || echo "Erreur lors de la création du répertoire, continuation du script..."
-mkdir -pv /config/cache || echo "Erreur lors de la création du répertoire, continuation du script..."
-
-sleep 15
-
 su squeezeboxserver -s /bin/sh -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
-# /bin/sh -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT $EXTRA_ARGS'
